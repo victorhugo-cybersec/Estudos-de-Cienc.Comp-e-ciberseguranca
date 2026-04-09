55563fe6dc2918916e2cc9ba64dfc1a71114099b
vh <victorhugo.ciberseg@gmail.com>
2026-04-09 18:17:50 -0300

    Anotações de ciências da computação. Inclui PDFs, resumos de livros, e estudos que frequentemente atualizo.

---
+++ Raciocínio lógico algorítmico/exercícios/exemplos para a aula de algoritmo dia 5 do 3.js
@@ -0,0 +1,13 @@
+
+
+let nomeDeVariavel = "Abóbora";
+
+let nome_de_variavel = "Com";
+
+let nomedevariavel = "Leite";
+
+if(nomeDeVariavel === "Abóbora") {
+    nomeDeVariavel = "Banana";
+}
+
+console.log(nomeDeVariavel);
